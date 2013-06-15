ce14632c2486283091a9731925611e2f7d9fd031
qhwa <qhwa@163.com>
2013-06-15 14:04:20 +0800

    add rails application

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DailyGoal::Application.config.secret_key_base = '9baebf8d9bb0d56b7ef662c87684ed8735e397bfe74142f846d12c20f0dca918e333f31b4f1185bca16dd3f13b2d9d55e604c3669a2452d9094f470e2c35aacc'
---
+++ webapp/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DailyGoal::Application.config.session_store :cookie_store, key: '_daily-goal_session'
