8945b620f998c9f0a0e806f08f874007378d4fc6
qhwa <qhwa@163.com>
2013-06-15 13:19:18 +0800

    添加故事版；添加webapp原型（rails程序）

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DailyGoal::Application.config.secret_token = '1191f07a93f301c43fff34fb511decb99984bdfb4f7055efcb9abdcd426d7da48bd02ea15753c4e161f7872983d25c24278093711ba7d0581f153b6973f6a4bf'
