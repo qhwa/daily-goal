f6a4a9a24cf2c52152247597b9cd6668c1fba40f
hua.qiuh <hua.qiuh@alibaba-inc.com>
2013-06-30 19:51:36 +0800

    validating reviews

--- webapp/app/models/daily_review.rb
+++ webapp/app/models/daily_review.rb
@@ -3,11 +3,21 @@ class DailyReview < ActiveRecord::Base
   belongs_to :task
 
   validates_presence_of :task_id, :date
+  validate :check_date_after_task_created
 
   attr_accessor :need_to_be_done
 
   def need_to_be_done?
     @need_to_be_done
   end
+
+  protected
+
+    # review的时间应该在任务创建之后
+    def check_date_after_task_created
+      if task.persisted? && date < task.created_at.to_date
+        errors.add :date, 'should after task created'
+      end
+    end
 
 end
